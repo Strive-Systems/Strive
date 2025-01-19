a5e163ac6cdc34e2216498b6e2c79b9a3f253485
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-01-19 01:28:17 +0000

    [UPDATE] Added ban, mute, softban, warn and kick protections on server admins and bypassed users.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -4,7 +4,7 @@ import uuid
 import re
 import time
 from discord.ext import commands
-from utils.constants import StriveConstants, cases
+from utils.constants import StriveConstants, cases, blacklist_bypass
 from utils.utils import get_next_case_id
 from datetime import timedelta
 from datetime import datetime
@@ -22,110 +22,146 @@ class ModerationCommandCog(commands.Cog):
     @commands.hybrid_command(description="You can run this command to warn a user in your server.", with_app_command=True, extras={"category": "Moderation"})
     @commands.has_permissions(administrator=True)
     async def warn(self, ctx, member: discord.Member, *, reason: str = "No reason provided"):
-        case_id = await get_next_case_id(ctx.guild.id)
+        
+        if await is_blacklisted_or_admin(ctx, member):
+            
+            
+            return await ctx.send(f"")
+                embed = discord.Embed(
+                title="",
+                description=f"<:error:1326752911870660704> You cannot warn {member.mention} because they are an admin or bypassed from moderation.",
+                color=discord.Color.green()
+            )
+            
+            
+            await ctx.send(embed=embed)
+            
+            
+        else:
+    
+        
+            case_id = await get_next_case_id(ctx.guild.id)
 
 
-        try:
-            dm_message = f"<:success:1326752811219947571> **Case #{case_id} - You have been warned in {ctx.guild.name}** for {reason}."
-            await member.send(dm_message)
-        except discord.Forbidden:
-            await ctx.send(f"<:error:1326752911870660704> Unable to send a DM to {member.mention}; warning the user in the server.")
-        
-        
-        warn_entry = {
-            "case_id": case_id,
-            "guild_id": ctx.guild.id,
-            "user_id": member.id,
-            "moderator_id": ctx.author.id,
-            "reason": reason,
-            "timestamp": int(time.time()),
-            "type": "warn",
-            "status": "active"
-        }
-        await cases.insert_one(warn_entry)
+            try:
+                dm_message = f"<:success:1326752811219947571> **Case #{case_id} - You have been warned in {ctx.guild.name}** for {reason}."
+                await member.send(dm_message)
+            except discord.Forbidden:
+                await ctx.send(f"<:error:1326752911870660704> Unable to send a DM to {member.mention}; warning the user in the server.")
+            
+            
+            warn_entry = {
+                "case_id": case_id,
+                "guild_id": ctx.guild.id,
+                "user_id": member.id,
+                "moderator_id": ctx.author.id,
+                "reason": reason,
+                "timestamp": int(time.time()),
+                "type": "warn",
+                "status": "active"
+            }
+            await cases.insert_one(warn_entry)
 
 
-        embed = discord.Embed(
-            title="",
-            description=f"<:success:1326752811219947571> **Case #{case_id} - {member}** has been warned for {reason}.",
-            color=discord.Color.green()
-        )
-        
-        
-        await ctx.send(embed=embed)
+            embed = discord.Embed(
+                title="",
+                description=f"<:success:1326752811219947571> **Case #{case_id} - {member}** has been warned for {reason}.",
+                color=discord.Color.green()
+            )
+            
+            
+            await ctx.send(embed=embed)
         
         
         
     @commands.hybrid_command(name="ban", description="Ban command to ban members from your server.", with_app_command=True, extras={"category": "Moderation"})
     @commands.has_guild_permissions(ban_members=True)
     async def ban(self, ctx: commands.Context, member: discord.User, *, reason: str = "Nothing was provided"):
-        try:
-            fetched_member: discord.Member = await self.strive.fetch_user(member.id)
-        except Exception as e:
-            raise commands.CommandInvokeError(e)
         
-        
-        banned_users = ctx.guild.bans()
-        user_to_unban = None
-        
-        
-        async for ban_entry in banned_users:
-            if ban_entry.user.id == fetched_member.id:
-                user_to_unban = ban_entry.user
-                break
-
-
-        # Moved the error checking to the top to prevent as many nested if statements.
-
-        if user_to_unban:
-            return await ctx.send(f"<:error:1326752911870660704> User {fetched_member} is already banned.", ephemeral=True)
-        
-        
-        elif fetched_member == ctx.author:
-            return await ctx.send("<:error:1326752911870660704> You cannot ban yourself!")
-    
-    
-        elif fetched_member == ctx.guild.me:
-            return await ctx.send("<:error:1326752911870660704> I cannot ban myself!")
-        
-        
-        try:
-            if fetched_member.top_role >= ctx.author.top_role:
-                return await ctx.send("You cannot ban a member with an equal or higher role!")
+        if await is_blacklisted_or_admin(ctx, member):
             
             
-        except AttributeError:
-            pass
-         
-        # Sends a DM to the user
-        
-        case_id = await get_next_case_id(ctx.guild.id)
-
-
-        # Perform the ban operation
-        try:
-            await ctx.guild.ban(fetched_member, reason=reason)
-        except Exception as e:
-            raise commands.CommandInvokeError(e)
-        
-        # Log to MongoDB
-        
-        ban_entry = {
-            "case_id": case_id,
-            "guild_id": ctx.guild.id,
-            "user_id": member.id,
-            "moderator_id": ctx.author.id,
-            "reason": reason,
-            "timestamp": int(time.time()),
-            "type": "ban",
-            "status": "active"
-        }
-        await cases.insert_one(ban_entry)
-
-        # Send the success message
-        
-        await ctx.send(f"<:success:1326752811219947571> **Case #{case_id} - {member}** has been banned for {reason}.")
+            return await ctx.send(f"")
+                embed = discord.Embed(
+                title="",
+                description=f"<:error:1326752911870660704> You cannot ban {member.mention} because they are an admin or bypassed from moderation.",
+                color=discord.Color.green()
+            )
             
+            
+            await ctx.send(embed=embed)
+            
+            
+        else:
+        
+        
+            try:
+                fetched_member: discord.Member = await self.strive.fetch_user(member.id)
+            except Exception as e:
+                raise commands.CommandInvokeError(e)
+            
+            
+            banned_users = ctx.guild.bans()
+            user_to_unban = None
+            
+            
+            async for ban_entry in banned_users:
+                if ban_entry.user.id == fetched_member.id:
+                    user_to_unban = ban_entry.user
+                    break
+
+
+            # Moved the error checking to the top to prevent as many nested if statements.
+
+            if user_to_unban:
+                return await ctx.send(f"<:error:1326752911870660704> User {fetched_member} is already banned.", ephemeral=True)
+            
+            
+            elif fetched_member == ctx.author:
+                return await ctx.send("<:error:1326752911870660704> You cannot ban yourself!")
+        
+        
+            elif fetched_member == ctx.guild.me:
+                return await ctx.send("<:error:1326752911870660704> I cannot ban myself!")
+            
+            
+            try:
+                if fetched_member.top_role >= ctx.author.top_role:
+                    return await ctx.send("You cannot ban a member with an equal or higher role!")
+                
+                
+            except AttributeError:
+                pass
+            
+            # Sends a DM to the user
+            
+            case_id = await get_next_case_id(ctx.guild.id)
+
+
+            # Perform the ban operation
+            try:
+                await ctx.guild.ban(fetched_member, reason=reason)
+            except Exception as e:
+                raise commands.CommandInvokeError(e)
+            
+            # Log to MongoDB
+            
+            ban_entry = {
+                "case_id": case_id,
+                "guild_id": ctx.guild.id,
+                "user_id": member.id,
+                "moderator_id": ctx.author.id,
+                "reason": reason,
+                "timestamp": int(time.time()),
+                "type": "ban",
+                "status": "active"
+            }
+            await cases.insert_one(ban_entry)
+
+            # Send the success message
+            
+            await ctx.send(f"<:success:1326752811219947571> **Case #{case_id} - {member}** has been banned for {reason}.")
+                
             
             
     @commands.hybrid_command(name="unban", description="Unban command to unban members from your server.", with_app_command=True, extras={"category": "Moderation"})
@@ -176,65 +212,99 @@ class ModerationCommandCog(commands.Cog):
     @commands.bot_has_permissions(ban_members=True)
     async def softban(self, ctx, member: discord.Member, *, reason: str = "No reason provided"):
         
+        if await is_blacklisted_or_admin(ctx, member):
+            
+            
+            return await ctx.send(f"")
+                embed = discord.Embed(
+                title="",
+                description=f"<:error:1326752911870660704> You cannot softban {member.mention} because they are an admin or bypassed from moderation.",
+                color=discord.Color.green()
+            )
+            
+            
+            await ctx.send(embed=embed)
+            
+        else:
 
-        await ctx.guild.ban(member, reason=reason, delete_message_days=1)
-        case_number = f"Case #{str(uuid.uuid4().int)[:4]}"  # Generate a short unique case number
-        await ctx.send(f"<:success:1326752811219947571> **Case #{case_number} - Successfully softbanned {member.mention}** for: {reason}")
-        await asyncio.sleep(2)
-        await ctx.guild.unban(member)
+            await ctx.guild.ban(member, reason=reason, delete_message_days=1)
+            case_number = f"Case #{str(uuid.uuid4().int)[:4]}"  # Generate a short unique case number
+            await ctx.send(f"<:success:1326752811219947571> **Case #{case_number} - Successfully softbanned {member.mention}** for: {reason}")
+            await asyncio.sleep(2)
+            await ctx.guild.unban(member)
         
     
     
     @commands.hybrid_command(description="Mute/Timeout a certain user", with_app_command=True, extras={"category": "Moderation"})
     @commands.has_permissions(moderate_members=True)
     async def mute(self, ctx: commands.Context, member: discord.Member, time: str, *, reason: str = "No reason provided"):
-        if member == ctx.author:
-            return await ctx.send("<:error:1326752911870660704> You cannot mute yourself!")
         
-        elif member == ctx.guild.me:
-            return await ctx.send("<:error:1326752911870660704> I cannot mute myself!")
-        
-        try:
-            if member.top_role >= ctx.author.top_role:
-                return await ctx.send("You cannot mute a member with an equal or higher role!")
-        except AttributeError:
-            pass
+        if await is_blacklisted_or_admin(ctx, member):
+            
+            
+            return await ctx.send(f"")
+                embed = discord.Embed(
+                title="",
+                description=f"<:error:1326752911870660704> You cannot mute {member.mention} because they are an admin or bypassed from moderation.",
+                color=discord.Color.green()
+            )
+            
+            
+            await ctx.send(embed=embed)
+            
 
-
-        time_match = re.match(r"(\d+)([mshd])", time)
-        if not time_match:
-            return await ctx.send("Invalid time format. Use `1m`, `1h`, etc.")
-
-
-        amount, unit = time_match.groups()
-        amount = int(amount)
-
-
-        if unit == "m":
-            delta = timedelta(minutes=amount)
-        elif unit == "h":
-            delta = timedelta(hours=amount)
-        elif unit == "s":
-            delta = timedelta(seconds=amount)
-        elif unit == "d":
-            delta = timedelta(days=amount)
         else:
-            return await ctx.send("Invalid time unit. Use `m`, `h`, `s`, or `d`.")
+    
+    
+            if member == ctx.author:
+                return await ctx.send("<:error:1326752911870660704> You cannot mute yourself!")
+            
+            
+            elif member == ctx.guild.me:
+                return await ctx.send("<:error:1326752911870660704> I cannot mute myself!")
+            
+            
+            try:
+                if member.top_role >= ctx.author.top_role:
+                    return await ctx.send("You cannot mute a member with an equal or higher role!")
+            except AttributeError:
+                pass
 
 
-        until = discord.utils.utcnow() + delta
-        await member.timeout(until, reason=reason)
-        formatted_time = discord.utils.format_dt(until, style="f")
-        
-        
-        embed = discord.Embed(
-            title="",
-            description=f"<:success:1326752811219947571> **{member.name}** has been muted until {formatted_time}!",
-            color=discord.Color.green()
-        )
-        
-        
-        await ctx.send(embed=embed)
+            time_match = re.match(r"(\d+)([mshd])", time)
+            if not time_match:
+                return await ctx.send("Invalid time format. Use `1m`, `1h`, etc.")
+
+
+            amount, unit = time_match.groups()
+            amount = int(amount)
+
+
+            if unit == "m":
+                delta = timedelta(minutes=amount)
+            elif unit == "h":
+                delta = timedelta(hours=amount)
+            elif unit == "s":
+                delta = timedelta(seconds=amount)
+            elif unit == "d":
+                delta = timedelta(days=amount)
+            else:
+                return await ctx.send("Invalid time unit. Use `m`, `h`, `s`, or `d`.")
+
+
+            until = discord.utils.utcnow() + delta
+            await member.timeout(until, reason=reason)
+            formatted_time = discord.utils.format_dt(until, style="f")
+            
+            
+            embed = discord.Embed(
+                title="",
+                description=f"<:success:1326752811219947571> **{member.name}** has been muted until {formatted_time}!",
+                color=discord.Color.green()
+            )
+            
+            
+            await ctx.send(embed=embed)
 
 
 
@@ -271,47 +341,70 @@ class ModerationCommandCog(commands.Cog):
     @commands.hybrid_command(description="You can run this command to kick a user in your server.", with_app_command=True, extras={"category": "Moderation"})
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason: str = "No reason provided"):
-        if not ctx.guild.me.guild_permissions.manage_messages:
-            await ctx.send("<:error:1326752911870660704> I do not have permission to manage messages.")
-            return
         
-        if not ctx.guild.me.guild_permissions.kick_members:
-            await ctx.send("<:error:1326752911870660704> I do not have permission to kick members.")
-            return
-
-        try:
-            await member.kick(reason=reason)
-        except discord.Forbidden:
-            await ctx.send("<:error:1326752911870660704> I do not have permission to kick that user.")
-            return
-        except discord.HTTPException:
-            await ctx.send("<:error:1326752911870660704> I couldn't kick this user.")
-            return
-
-        case_id = await get_next_case_id(ctx.guild.id)
-
-        try:
-            dm_message = f"<:success:1326752811219947571> **Case #{case_id} - You have been kicked from **{ctx.guild.name}** for {reason}"
-            await member.send(dm_message)
-        except discord.Forbidden:
-            await ctx.send(f"<:error:1326752911870660704> Unable to send a DM to {member.mention}; kicking the user in the server.")
-
-
-        kick_entry = {
-            "case_id": case_id,
-            "guild_id": ctx.guild.id,
-            "user_id": member.id,
-            "moderator_id": ctx.author.id,
-            "reason": reason,
-            "timestamp": int(time.time()),
-            "type": "kick",
-            "status": "active"
-        }
-        await cases.insert_one(kick_entry)
         
-
-        await ctx.send(f"<:success:1326752811219947571> **Case #{case_id} - {member}** has been kicked for {reason}.")
+        if await is_blacklisted_or_admin(ctx, member):
+            
+            
+            return await ctx.send(f"")
+                embed = discord.Embed(
+                title="",
+                description=f"<:error:1326752911870660704> You cannot kick {member.mention} because they are an admin or bypassed from moderation.",
+                color=discord.Color.green()
+            )
+            
+            
+            await ctx.send(embed=embed)
+            
+            
+        else:
         
+        
+            if not ctx.guild.me.guild_permissions.manage_messages:
+                await ctx.send("<:error:1326752911870660704> I do not have permission to manage messages.")
+                return
+            
+            
+            if not ctx.guild.me.guild_permissions.kick_members:
+                await ctx.send("<:error:1326752911870660704> I do not have permission to kick members.")
+                return
+
+
+            try:
+                await member.kick(reason=reason)
+            except discord.Forbidden:
+                await ctx.send("<:error:1326752911870660704> I do not have permission to kick that user.")
+                return
+            except discord.HTTPException:
+                await ctx.send("<:error:1326752911870660704> I couldn't kick this user.")
+                return
+
+
+            case_id = await get_next_case_id(ctx.guild.id)
+
+
+            try:
+                dm_message = f"<:success:1326752811219947571> **Case #{case_id} - You have been kicked from **{ctx.guild.name}** for {reason}"
+                await member.send(dm_message)
+            except discord.Forbidden:
+                await ctx.send(f"<:error:1326752911870660704> Unable to send a DM to {member.mention}; kicking the user in the server.")
+
+
+            kick_entry = {
+                "case_id": case_id,
+                "guild_id": ctx.guild.id,
+                "user_id": member.id,
+                "moderator_id": ctx.author.id,
+                "reason": reason,
+                "timestamp": int(time.time()),
+                "type": "kick",
+                "status": "active"
+            }
+            await cases.insert_one(kick_entry)
+            
+
+            await ctx.send(f"<:success:1326752811219947571> **Case #{case_id} - {member}** has been kicked for {reason}.")
+            
         
         
     @commands.hybrid_group(description="Group command")
@@ -516,6 +609,22 @@ class ModerationCommandCog(commands.Cog):
         
         
         await ctx.send(embed=embed)
+
+
+
+    async def is_blacklisted_or_admin(ctx, member: discord.Member):
+
+
+        if member.guild_permissions.administrator:
+            return True
+        
+
+        blacklist_entry = await blacklist_bypass.find_one({"user_id": member.id, "guild_id": ctx.guild.id})
+        if blacklist_entry:
+            return True
+
+
+        return False
 
 
 
