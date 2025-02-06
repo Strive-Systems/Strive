7254b67402251ccfe57b1de1cc7bfb9c53d1ce9b
NickDerry2023 <nickd@caliwebdesignservices.com>
2025-02-06 06:50:52 +0000

    [UPDATE] Forgot a couple of imports and added reminder_counters to utils file.

--- utils/constants.py
+++ utils/constants.py
@@ -24,6 +24,7 @@ setup_col = db.setup
 verify_waiting = db.verify_waiting
 cases = db.cases
 guild_counters = db.guild_counters
+reminder_counters = db.reminder_counters
 timezones = db.timezones
 
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,22 +1,31 @@
 import discord
 from discord.ext import commands
-from utils.constants import guild_counters, prefixes
+from utils.constants import guild_counters, reminder_counters, prefixes
 import os
 from dotenv import load_dotenv
-
 
 async def get_next_case_id(guild_id):
     
     guild_case_number = await guild_counters.find_one_and_update(
-        {"_id": str(guild_id)},  # Search by guild ID
-        {"$inc": {"seq": 1}},  # Increment the case ID by 1
-        upsert=True,  # Create the document if it doesn't exist
-        return_document=True  # Return the updated document
+        {"_id": str(guild_id)},
+        {"$inc": {"seq": 1}},
+        upsert=True,
+        return_document=True
     )
     
-    # Return the updated sequence number for this guild
     return guild_case_number["seq"]
 
+
+async def get_next_reminder_id(guild_id):
+    
+    guild_reminder_number = await reminder_counters.find_one_and_update(
+        {"_id": str(guild_id)},
+        {"$inc": {"seq": 1}},
+        upsert=True,
+        return_document=True
+    )
+    
+    return guild_reminder_number["seq"]
 
 
 async def get_prefix(strive, message):
